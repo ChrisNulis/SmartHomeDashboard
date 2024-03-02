1afd3017322e47a88c2514d84405764ca2641f76
ChrisNulis <chrisnulis@gmail.com>
2024-03-02 18:12:43 -0500

    Header completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,28 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Header = () => {
   return (
-    <View style={[styles.container, { backgroundColor: 'blue' }]}>
-      <Text style={styles.title}>This is Header</Text>
+    <View style={styles.container}>
+      <View style={styles.dot} />
+      <Text style={styles.title}>LIVE FORECAST</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    backgroundColor: 'green',
+    borderRadius: 5,
+    marginRight: 10,
   },
   title: {
     color: 'white',
-    fontSize: 24,
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
