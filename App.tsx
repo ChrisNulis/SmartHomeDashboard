cd11c1695a8fbb65519588a6e6c40e3a12a5aad2
ChrisNulis <chrisnulis@gmail.com>
2024-03-04 00:14:07 -0500

    adding scrolling capability

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Header from './src/components/Header/Header';
 import Weather from './src/components/Weather/Weather';
 import Scenes from './src/components/Scenes/Scenes';
@@ -9,19 +9,24 @@ import { devices } from './src/dataMockUp';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.contentContainer}>
-        <Weather />
-        <Scenes />
-        <Devices />
-        <Cards devices={devices} />
+    <ScrollView contentContainerStyle={styles.scrollView}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.contentContainer}>
+          <Weather />
+          <Scenes />
+          <Devices />
+          <Cards devices={devices} />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: 'black',
@@ -29,7 +34,6 @@ const styles = StyleSheet.create({
     paddingTop: 50,
   },
   contentContainer: {
-    flex: 1,
     marginTop: 20,
   },
 });
